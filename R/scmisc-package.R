a793653a9a6bc7da81af1d64eee9517c2d28ddb8
Diego Diez <diego10ruiz@gmail.com>
2018-09-21 11:57:59 +0900

    Fix dependencies.

--- R/scmisc-package.R
+++ R/scmisc-package.R
@@ -3,5 +3,8 @@
 #' @name scmisc
 #' @docType package
 #'
+#' @import Biobase SummarizedExperiment SingleCellExperiment
 #' @importFrom monocle newCellDataSet
+#' @importFrom VGAM negbinomial.size
+#' @importFrom dplyr %>% rename
 NULL
