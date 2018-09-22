9d8eca6655db63a28391899e59d4c54ed497dd67
Diego Diez <diego10ruiz@gmail.com>
2018-09-22 23:54:52 +0900

    Add S3 methods for SingleCellExperiment and CellDataSet to get_density().

--- R/density.R
+++ R/density.R
@@ -6,10 +6,30 @@
 #'
 #' @note I took this implementation from http://slowkow.com/notes/ggplot2-color-by-density/
 #' @export
-get_density <- function(x, y, n = 100) {
+get_density <- function(x, ...) {
+  UseMethod("get_density")
+}
+
+#' @rdname get_density
+#' @export
+get_density.default <- function(x, y, n = 100) {
   dens <- MASS::kde2d(x = x, y = y, n = n)
   ix <- findInterval(x, dens$x)
   iy <- findInterval(y, dens$y)
   ii <- cbind(ix, iy)
   dens$z[ii]
+}
+
+#' @rdname get_density
+#' @export
+get_density.SingleCellExperiment <- function(x, name = "tSNE") {
+  y <- get_coord(x, name = name, annotate = FALSE)
+  get_density(y[, 1], y[, 2])
+}
+
+#' @rdname get_density
+#' @export
+get_density.CellDataSet <- function(x, name = "A") {
+  y <- get_coord(x, name = name, annotate = FALSE)
+  get_density(y[, 1], y[, 2])
 }
