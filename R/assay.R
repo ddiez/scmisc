40850eeb8078ca37095bfc026067c26d83529394
Diego Diez <diego10ruiz@gmail.com>
2018-09-22 12:00:05 +0900

    Fix documentation.

--- R/assay.R
+++ R/assay.R
@@ -1,5 +1,6 @@
 #' Compute summary from assay matrix
 #'
+#' @param x object to compute summary.
 #' @param sum.fun summary function (by default mean).
 #' @param name name of the feature value for which to compute the summary.
 #' @param column name of the column where to find 'name' (default: 'symbol').
@@ -7,13 +8,13 @@
 #' @param ... arguments passed down to specific methods.
 #'
 #' @export
-get_assay_summary <- function(x, sum.fun = mean, ...) {
+get_assay_summary <- function(x, ...) {
   UseMethod("get_assay_summary")
 }
 
 #' @rdname get_assay_summary
 #' @export
-get_assay_summary.SingleCellExperiment <- function(x, name, assay.name = "logcounts", column = "symbol", sum.fun = mean) {
+get_assay_summary.SingleCellExperiment <- function(x, name, assay.name = "logcounts", column = "symbol", sum.fun = mean, ...) {
   y <- assay(x, assay.name)
   z <- y[rowData(x)[[column]] == name, , drop = FALSE]
   apply(z, 2, sum.fun)
