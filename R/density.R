83c12188e06e62b3ca7c618aaf96b29b7fd6accb
Diego Diez <diego10ruiz@gmail.com>
2018-10-04 14:04:16 +0900

    Rename get_density() to compute_density().

--- R/density.R
+++ R/density.R
@@ -1,4 +1,4 @@
-#' get_density
+#' compute_density
 #'
 #' @param x x coordinate of data
 #' @param y y coordinate of data
@@ -8,13 +8,13 @@
 #'
 #' @note I took this implementation from http://slowkow.com/notes/ggplot2-color-by-density/
 #' @export
-get_density <- function(x, ...) {
-  UseMethod("get_density")
+compute_density <- function(x, ...) {
+  UseMethod("compute_density")
 }
 
-#' @rdname get_density
+#' @rdname compute_density
 #' @export
-get_density.default <- function(x, y, n = 100, ...) {
+compute_density.default <- function(x, y, n = 100, ...) {
   dens <- MASS::kde2d(x = x, y = y, n = n)
   ix <- findInterval(x, dens$x)
   iy <- findInterval(y, dens$y)
@@ -22,16 +22,16 @@ get_density.default <- function(x, y, n = 100, ...) {
   dens$z[ii]
 }
 
-#' @rdname get_density
+#' @rdname compute_density
 #' @export
-get_density.SingleCellExperiment <- function(x, name = "tSNE", ...) {
+compute_density.SingleCellExperiment <- function(x, name = "tSNE", ...) {
   y <- get_coord(x, name = name, annotate = FALSE)
-  get_density(y[, 1], y[, 2])
+  compute_density(y[, 1], y[, 2])
 }
 
-#' @rdname get_density
+#' @rdname compute_density
 #' @export
-get_density.CellDataSet <- function(x, name = "A", ...) {
+compute_density.CellDataSet <- function(x, name = "A", ...) {
   y <- get_coord(x, name = name, annotate = FALSE)
-  get_density(y[, 1], y[, 2])
+  compute_density(y[, 1], y[, 2])
 }
