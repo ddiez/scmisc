f3d498fe368be07e17cfc65caa7061b9a1552350
Diego Diez <diego10ruiz@gmail.com>
2018-10-17 17:28:03 +0900

    Fix dependencies.

--- R/scmisc-package.R
+++ R/scmisc-package.R
@@ -3,11 +3,11 @@
 #' @name scmisc
 #' @docType package
 #'
-#' @import Biobase SummarizedExperiment SingleCellExperiment
+#' @import Biobase SummarizedExperiment SingleCellExperiment ggplot2
 #' @importFrom monocle newCellDataSet clusterCells reducedDimA<-
 #' @importFrom VGAM negbinomial.size
 #' @importFrom MASS kde2d
-#' @importFrom dplyr %>% rename
+#' @importFrom dplyr %>% rename filter
 #' @importFrom stats prcomp kmeans dist hclust cutree
 #' @importFrom Rtsne Rtsne
 #' @importFrom scran buildSNNGraph
