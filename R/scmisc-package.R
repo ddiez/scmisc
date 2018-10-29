26afdda28c82e75383001bbd121d0a2ef96a75c4
Diego Diez <diego10ruiz@gmail.com>
2018-10-29 18:18:02 +0900

    Import all dplyr NAMESPACE and avoid class with Biobase (combine).

--- R/scmisc-package.R
+++ R/scmisc-package.R
@@ -4,10 +4,10 @@
 #' @docType package
 #'
 #' @import tibble Biobase SummarizedExperiment SingleCellExperiment
+#' @rawNamespace import(Biobase, except = combine)
 #' @importFrom monocle newCellDataSet clusterCells reducedDimA<-
 #' @importFrom VGAM negbinomial.size
 #' @importFrom MASS kde2d
-#' @importFrom dplyr %>% rename filter bind_cols sample_n pull group_by_
 #' @importFrom tidyr gather
 #' @importFrom stats prcomp kmeans dist hclust cutree
 #' @importFrom Rtsne Rtsne
