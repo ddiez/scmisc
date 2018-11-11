97f7900df3a0d5d49c97374b1b3869a373c69417
Diego Diez <diego10ruiz@gmail.com>
2018-11-11 20:30:21 +0900

    Add plot_coord() methods for SingleCellExperiment and data.frame objects.

--- R/plot.R
+++ R/plot.R
@@ -1,3 +1,57 @@
+#' plot_coord
+#'
+#' @param x object from which to plot coordinates.
+#' @param size size of points in geom_point()
+#' @param color color or column to color by.
+#' @param label name of column used to label plot (e.g. clusters).
+#' @param expand logical; whether to expand one column to show presence/absence.
+#' @param ... further arguments passed down to get_coord().
+#'
+#' @export
+plot_coord <- function(x, ...) {
+  UseMethod("plot_coord")
+}
+
+#' @rdname plot_coord
+#' @export
+plot_coord.SingleCellExperiment <- function(x, size = .1, color = NULL, label = NULL, expand = NULL, ...) {
+  d <- get_coord(x, ...)
+  plot_coord(d, size = size, color = color, label = label, expand = expand, ...)
+}
+
+#' @rdname plot_coord
+#' @export
+plot_coord.data.frame <- function(x, size = .1, color = NULL, label = NULL, expand = NULL, ...) {
+  d <- x
+  if (!is.null(expand)) {
+    d <- d %>% expand_column(expand)
+    p <- ggplot(d, aes_string("dim1", "dim2", color = "value")) +
+      geom_point(size = size) +
+      scale_color_manual(values = c("grey", "red")) +
+      facet_wrap(~.data[[expand]])
+  } else {
+    if (!is.null(color))
+      d <- d %>% arrange_(color)
+
+    p <- ggplot(d, aes_string("dim1", "dim2")) +
+      geom_point(size = size)
+
+    if (!is.null(color)) {
+      p <- p + aes_string(color = color)
+      if (is.numeric(d[[color]]))
+        p <- p + scale_color_gradient(low = "grey", high = "red")
+    }
+
+    if (!is.null(label)) {
+      dd <- d %>% group_by_(label) %>%
+        summarize(dim1 = mean(.data$dim1), dim2 = mean(.data$dim2))
+      p <- p + geom_text(aes_string(label = label), data = dd, color = "black")
+    }
+  }
+
+  p
+}
+
 #' plot_purity
 #'
 #' @param x a suitable object.
