a1ee88fe8b9590e40fabb4db42afb7fbca457b2d
Diego Diez <diego10ruiz@gmail.com>
2019-04-15 15:36:23 +0900

    Add get_rowdata() and get_coldata() methods for Seurat object.

--- R/data.R
+++ R/data.R
@@ -13,10 +13,15 @@ get_coldata.SingleCellExperiment <- function(x) {
   colData(x) %>% as.data.frame()
 }
 
-
 #' @rdname get_coldata
 #' @export
 get_coldata.seurat <- function(x) {
+  x@meta.data %>% as.data.frame()
+}
+
+#' @rdname get_coldata
+#' @export
+get_coldata.Seurat <- function(x) {
   x@meta.data %>% as.data.frame()
 }
 
@@ -36,9 +41,14 @@ get_rowdata.SingleCellExperiment <- function(x) {
   rowData(x) %>% as.data.frame()
 }
 
-
 #' @rdname get_rowdata
 #' @export
 get_rowdata.seurat <- function(x) {
   data.frame(symbol = rownames(x@data))
+}
+
+#' @rdname get_rowdata
+#' @export
+get_rowdata.Seurat <- function(x) {
+  data.frame(symbol = rownames(x))
 }
