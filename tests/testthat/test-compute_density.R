cd264e73852d6c2c3f582f60fc71e39a0ba88a39
Diego Diez <diego10ruiz@gmail.com>
2019-10-01 13:46:18 +0900

    Fix name of test.

--- tests/testthat/test-compute_density.R
+++ tests/testthat/test-compute_density.R
@@ -2,7 +2,7 @@ context("compute_density")
 
 x <- compute_density(mtcars[, 1], mtcars[, 2])
 
-test_that("multiplication works", {
+test_that("compute_density works", {
   expect_is(x, "numeric")
   expect_equal(length(x), nrow(mtcars))
 })
