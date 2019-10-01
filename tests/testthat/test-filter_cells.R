240a29dd1a78b8f5343db892d57112bbb7cc49ba
Diego Diez <diego10ruiz@gmail.com>
2019-10-01 13:46:59 +0900

    Fix example dataset by removing features with zero counts in all cells. Fixed affected tests.

--- tests/testthat/test-filter_cells.R
+++ tests/testthat/test-filter_cells.R
@@ -4,17 +4,17 @@ x <- scmisc::sce
 x <- filter_cells(x)
 
 test_that("filter_cells works", {
-  expect_equal(dim(x), c(461, 493))
+  expect_equal(dim(x), c(434, 493))
 })
 
 x <- filter_cells(x, min.genes = 100)
 
 test_that("filter_cells works", {
-  expect_equal(dim(x), c(461, 430))
+  expect_equal(dim(x), c(434, 430))
 })
 
 x <- filter_cells(x, min.count = 10)
 
 test_that("filter_cells works", {
-  expect_equal(dim(x), c(461, 427))
+  expect_equal(dim(x), c(434, 427))
 })
--- tests/testthat/test-get_rowdata.R
+++ tests/testthat/test-get_rowdata.R
@@ -3,5 +3,5 @@ context("test-get_rowdata")
 x <- get_rowdata(scmisc::sce)
 
 test_that("get_rowdata works", {
-  expect_equal(dim(x), c(461, 4))
+  expect_equal(dim(x), c(434, 4))
 })
