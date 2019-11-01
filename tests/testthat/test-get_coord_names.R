ab33e5387d2377857cf8ecbb568b3978aaa283b2
Diego Diez <diego10ruiz@gmail.com>
2019-11-01 13:09:11 +0900

    Fix get_coord_names() test.

--- tests/testthat/test-get_coord_names.R
+++ tests/testthat/test-get_coord_names.R
@@ -7,7 +7,7 @@ y2 <- get_coord_names(x)
 
 test_that("get_coord_names works", {
   expect_equal(length(y1), 0)
-  expect_equal(y1, NULL)
+  expect_equal(y1, character(0))
   expect_equal(length(y2), 1)
   expect_equal(y2, "pca")
 })
