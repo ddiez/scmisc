6c1fbafc7f42376332f578022e97d9686eb336c0
Diego Diez <diego10ruiz@gmail.com>
2020-02-25 14:08:37 +0900

    Add compute_percentage_variance().

---
+++ R/variance.R
@@ -0,0 +1,24 @@
+#' compute_percentage_variance
+#'
+#' @param x and object with PCA coordintates
+#' @param reduction name of the reduction to use with Seurat objects.
+#'
+#' @export
+#'
+compute_percentage_variance <- function(x, reduction = "pca") {
+  UseMethod("compute_percentage_variance")
+}
+
+
+#' @rdname compute_percentage_variance
+#' @export
+compute_percentage_variance.Seurat <- function(x, reduction = "pca") {
+  emb <- Embeddings(x, reduction = reduction)
+  stdev <- Stdev(x, reduction = reduction)
+
+
+  d <- tibble(dims = seq_len(ncol(emb)), stdev = stdev, variance = stdev ^ 2) %>%
+    add_tally(.data$variance, name = "total") %>%
+    mutate(percentage = 100 * .data$variance / .data$total)
+}
+
