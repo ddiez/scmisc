4205056e88ff4123a438e852710181f9727c8d32
Diego Diez <diego10ruiz@gmail.com>
2020-04-29 19:34:35 +0900

    Usethis template for package doc.

--- R/scmisc-package.R
+++ R/scmisc-package.R
@@ -1,8 +1,9 @@
-#' scmisc.
-#'
-#' @name scmisc
-#' @docType package
-#'
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
 #' @import tibble dplyr ggplot2 ggraph tidygraph SummarizedExperiment SingleCellExperiment
 #' @rawNamespace import(Biobase, except = combine)
 #' @importFrom monocle newCellDataSet clusterCells reducedDimA<-
@@ -19,4 +20,5 @@
 #' @importFrom scran buildSNNGraph
 #' @importFrom igraph cluster_louvain graph_from_adjacency_matrix
 #' @importFrom ggbeeswarm geom_quasirandom
+## usethis namespace: end
 NULL
