1a1f63a8faa46910caa3c1d4f599cd37c106f090
Diego Diez <diego10ruiz@gmail.com>
2021-07-14 17:06:54 +0900

    Better be safe than sorry.

--- R/assay.R
+++ R/assay.R
@@ -25,7 +25,7 @@ get_assay_summary.SingleCellExperiment <- function(x, name, assay.name = "logcou
 #' @export
 get_assay_summary.Seurat <- function(x, name, assay.name = NULL, slot = "data", column = "symbol", sum.fun = mean, ...) {
   if (is.null(assay.name)) assay.name <- DefaultAssay(x)
-  y <- GetAssayData(x, slot, assay.name)
+  y <- GetAssayData(x, slot = slot, assay = assay.name)
   z <- y[get_rowdata(x, assay = assay.name)[[column]] == name, , drop = FALSE]
   apply(z, 2, sum.fun)
 }
