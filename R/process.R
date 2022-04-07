33c9b7750e2c68cf33b09b329c5a71dde1fe2c92
Diego Diez <diego10ruiz@gmail.com>
2022-04-07 17:20:38 +0900

    Add arguments to process.Seurat to pass down to RunUMAP.

--- R/process.R
+++ R/process.R
@@ -8,6 +8,10 @@
 #' @param algorithm algorithm to use for clustering.
 #' @param resolution resolution to use for clustering.
 #' @param nfeatures number of features for FindVariableFeatures().
+#' @param metric metric used for UMAP.
+#' @param n.neighbors number of nearest-neighbors to use for UMAP.
+#' @param min.dist min.dist for UMAP.
+#' @param spread spread for UMAP.
 #' @param verbose whether to output diagnostic information.
 #' @param ... arguments passed down to methods.
 #'
@@ -18,7 +22,7 @@ process <- function(x, ...) {
 
 #' @rdname process
 #' @export
-process.Seurat <- function(x, assay = NULL, dims = 1:10, algorithm = 1, resolution = 0.6, nfeatures = 2000, verbose = FALSE, ...) {
+process.Seurat <- function(x, assay = NULL, dims = 1:10, algorithm = 1, resolution = 0.6, nfeatures = 2000, metric="cosine", n.neighbors=30L, min.dist=0.3, spread=1, verbose = FALSE, ...) {
   if (!is.null(assay)) {
     old.assay <- SeuratObject::DefaultAssay(x)
     DefaultAssay(x) <- assay
@@ -26,7 +30,7 @@ process.Seurat <- function(x, assay = NULL, dims = 1:10, algorithm = 1, resoluti
   x <- FindVariableFeatures(x, nfeatures = nfeatures, verbose = verbose)
   x <- ScaleData(x, verbose = verbose)
   x <- RunPCA(x, verbose = verbose)
-  x <- RunUMAP(x, dims = dims, verbose = verbose)
+  x <- RunUMAP(x, dims = dims, metric=metric, spread=spread, min.dist=min.dist, n.neighbors=n.neighbors, verbose = verbose)
   x <- FindNeighbors(x, dims = dims, verbose = verbose)
   x <- FindClusters(x, algorithm = algorithm, resolution = resolution, verbose = verbose)
 
