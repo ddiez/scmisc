97d563969d2396e859747d21c9661c6e6d1ed3aa
Diego Diez <diego10ruiz@gmail.com>
2022-04-16 17:27:59 +0900

    Add plot_result_rank() for singleCellHaystack.

--- R/haystack.R
+++ R/haystack.R
@@ -1,12 +1,24 @@
+#' plot_result_rank
+#'
+#' @param x haystack object or summary data.frame.
+#' @param highlight gene names to highlight.
+#' @param sort.by rank the genes by the following column from the summary data.frame.
+#'
+#' @export
+#'
 plot_result_rank <- function(x, highlight=NULL, sort.by="log.p.adj") {
   UseMethod("plot_result_rank")
 }
 
+#' @rdname plot_result_rank
+#' @export
 plot_result_rank.haystack <- function(x, highlight=NULL, sort.by="log.p.adj") {
   sum <- show_result_haystack(x)
   plot_result_rank(sum, highlight=highlight)
 }
 
+#' @rdname plot_result_rank
+#' @export
 plot_result_rank.data.frame <- function(x, highlight=NULL, sort.by="log.p.adj") {
   x <- x |> rownames_to_column("gene")
   x <- x |> arrange(sort.by)
