90dcc41d44e98c052140d3adb0118f79aef0adfe
Diego Diez <diego10ruiz@gmail.com>
2022-08-21 23:47:07 +0900

    Separate adjacency matrix computation for plot_cpdb_heatmap() into calculate_cpdb_cell_graph() function.

--- R/cpdb.R
+++ R/cpdb.R
@@ -101,17 +101,20 @@ plot_cpdb_dotplot <- function(x, cutoff=1e-3, filter=NULL, cells=NULL, proteins=
 #' @export
 #'
 plot_cpdb_heatmap <- function(x, cutoff=1e-3) {
+  d <- calculate_cpdb_cell_graph(x, cutoff=cutoff)
+  ggplot(d, aes(.data[["cell_b"]], .data[["cell_a"]], fill=.data[["score"]])) +
+    geom_tile() +
+    scale_fill_distiller(palette="Spectral") +
+    Seurat::RotatedAxis()
+}
+
+calculate_cpdb_cell_graph <- function(x, cutoff=1e-3) {
   res <- colSums(x[["m_pval"]] <= cutoff)
   d <- data.frame(cells=names(res), score=res)
   d <- d |> separate(col="cells", into=c("cell_a", "cell_b"), sep="\\|")
   m <- d |> pivot_wider(names_from="cell_b", values_from="score", values_fill=0) |> column_to_rownames("cell_a")
   o <- hclust(dist(m))$order
   cell_order <- rownames(m)[o]
-  d <- d |>
-    mutate(cell_a=factor(.data[["cell_a"]], levels=cell_order)) |>
+  d |> mutate(cell_a=factor(.data[["cell_a"]], levels=cell_order)) |>
     mutate(cell_b=factor(.data[["cell_b"]], levels=cell_order))
-  ggplot(d, aes(.data[["cell_b"]], .data[["cell_a"]], fill=.data[["score"]])) +
-    geom_tile() +
-    scale_fill_distiller(palette="Spectral") +
-    Seurat::RotatedAxis()
 }
