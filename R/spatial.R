36c206256958bc09294a43a08d7a86a2123384c7
Diego Diez <diego10ruiz@gmail.com>
2022-12-07 16:02:46 +0900

    Add rotation argument to plot_spatial().

--- R/spatial.R
+++ R/spatial.R
@@ -1,12 +1,12 @@
 #' @rdname plot_spatial
 #' @export
-plot_spatial <- function(x, features, size=.5) {
+plot_spatial <- function(x, features, size=.5, rotate=FALSE) {
   UseMethod("plot_spatial")
 }
 
 #' @rdname plot_spatial
 #' @export
-plot_spatial.Seurat <- function(x, features, size=.5) {
+plot_spatial.Seurat <- function(x, features, size=.5, rotate=FALSE) {
   images <- SeuratObject::Images(x)
   p <- vector("list", length(images) * length(features))
   k <- 1
@@ -19,10 +19,18 @@ plot_spatial.Seurat <- function(x, features, size=.5) {
     sel <- grep(paste0(image, "_"), colnames(exprs))
     d <- cbind(coord, Matrix::t(exprs[, sel, drop=FALSE]))
 
+    if (rotate) {
+      d$x <- -d$imagerow
+      d$y <- -d$imagecol
+    }
+    else {
+      d$x <- d$imagecol
+      d$y <- -d$imagerow
+    }
+
     for (feature in features) {
-      p[[k]] <- ggplot(d, aes(-imagerow, imagecol, fill=.data[[feature]])) +
+      p[[k]] <- ggplot(d, aes(x, y, fill=.data[[feature]])) +
         geom_point(shape=21, size=size, stroke=.1) +
-        coord_flip() +
         scale_fill_gradientn(colors=Seurat:::SpatialColors(1000)) + #, limits=c(NA, maxlim[feature])) +
         labs(title=image, subtitle=feature) +
         theme(axis.line=element_blank(), axis.text=element_blank(), axis.ticks=element_blank(), axis.title=element_blank())
