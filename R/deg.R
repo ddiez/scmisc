b035128e956eedd17e8c8d1bbc7a87a8d187f11e
Diego Diez <diego10ruiz@gmail.com>
2022-12-12 16:45:48 +0900

    Change default FDR for plot_deg_barplot to 0.01.

--- R/deg.R
+++ R/deg.R
@@ -22,7 +22,7 @@ top_deg <- function(x, fdr = 0.01, lfc = 1, n = 10, group = "cluster") {
 #' @param group column name to group results.
 #'
 #' @export
-plot_deg_barplot <- function(x, lfc=1, fdr=0.05, group="cluster") {
+plot_deg_barplot <- function(x, lfc=1, fdr=0.01, group="cluster") {
   d <- x |> filter(p_val_adj < fdr, abs(avg_log2FC) > lfc) |>
     mutate(direction=ifelse(avg_log2FC>0, "Up", "Down")) |>
     mutate(direction=factor(.data[["direction"]], levels=c("Up", "Down"))) |>
