93b3102315c636f2d1437e40891b11df8c9cf99b
Diego Diez <diego10ruiz@gmail.com>
2024-07-18 17:49:23 +0900

    More tests.

--- R/pseudobulk.R
+++ R/pseudobulk.R
@@ -58,20 +58,21 @@ pseudobulk.Seurat <- function(x, split.by, group.by, samples=NULL, genes=NULL, a
   })
 
   dge <- lapply(names(xl), function(n) {
-    s <- samples
-    s$cluster <- n
-    tmp <- edgeR::DGEList(xl[[n]])#, samples=s)
-    tmp$genes <- genes
+    #s <- samples
+    #s$cluster <- n
+    #tmp <- edgeR::DGEList(xl[[n]])#, samples=s)
+    #tmp$genes <- genes
+    tmp <- xl[[n]]
     colnames(tmp) <- unname(colnames(tmp))
     tmp
   })
   names(dge) <- names(xl)
 
-  if (sort_samples) {
-    dge <- lapply(dge, function(x) {
-      x[, sort(rownames(samples))]
-    })
-  }
+  #if (sort_samples) {
+  #  dge <- lapply(dge, function(x) {
+  #    x[, sort(rownames(samples))]
+  #  })
+  #}
 
   dge
 }
